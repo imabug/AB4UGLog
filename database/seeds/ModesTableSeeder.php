36cbd14d1c1cdf4e70c256b000d88df92eaf1fc9
Eugene Mah <eugenemah@gmail.com>
2017-05-13 13:14:35 -0400

    Modes table seeder
    Seed the modes table with modes from the ADIF spec
    http://www.adif.org.uk/305/ADIF_305.htm#Mode_Enumeration

---
+++ database/seeds/ModesTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use AB4UGLog\Mode;
+use Illuminate\Database\Seeder;
+
+class ModesTableSeeder extends Seeder
+{
+    /**
+     * Seed the modes table with modes defined in the ADIF 3.0.5 spec.
+     * http://www.adif.org.uk/305/ADIF_305.htm#Mode_Enumeration
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $modes = [
+            'AM', 'ARDOP', 'ATV', 'CHIP', 'CLO', 'CONTESTI', 'CW', 'DIGITALVOICE',
+            'DOMINO', 'DSTAR', 'FAX', 'FM', 'FSK441', 'HELL', 'ISCAT', 'JT4',
+            'JT6M', 'JT9', 'JT44', 'JT65', 'MFSK', 'MSK144', 'MT63', 'OLIVIA',
+            'OPERA', 'PAC', 'PAX', 'PKT', 'PSK', 'PSK2K', 'Q15', 'QRA64', 'ROS',
+            'RTTY', 'RTTYM', 'SSB', 'SSTV', 'THOR', 'THRB', 'TOR', 'V4', 'VOI',
+            'WINMOR', 'WSPR', 'AMTORFEC', 'ASCI', 'CHIP64', 'CHIP128', 'DOMINOF',
+            'FMHELL', 'FSK31', 'GTOR', 'HELL80', 'HFSK', 'JT4A', 'JT4B', 'JT4C',
+            'JT4D', 'JT4E', 'JT4F', 'JT4G', 'JT65A', 'JT65B', 'JT65C', 'MFSK8',
+            'MFSK16', 'PAC2', 'PAC3', 'PAX2', 'PCW', 'PSK10', 'PSK31', 'PSK63',
+            'PSK63F', 'PSK125', 'PSKAM10', 'PSKAM31', 'PSKAM50', 'PSKFEC31',
+            'PSKHELL', 'QPSK31', 'QPSK63', 'QPSK125', 'THRBX'
+        ];
+
+        foreach ($modes as $m) {
+            $mode = Mode::firstOrNew(['mode' => $m]);
+            if (! $mode->exists) {
+                $mode->fill(['mode' => $m])->save();                
+            }
+        }
+    }
+}
