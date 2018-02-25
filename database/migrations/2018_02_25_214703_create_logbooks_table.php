aafdc67bc6d9ffd66da56c8a4a87025044b8cbd1
Eugene Mah <eugenemah@gmail.com>
2018-02-25 17:02:44 -0500

    Pluralize logbooks table

--- database/migrations/2018_02_25_214703_create_logbooks_table.php
+++ database/migrations/2018_02_25_214703_create_logbooks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLogbookTable extends Migration
+class CreateLogbooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateLogbookTable extends Migration
      */
     public function up()
     {
-        Schema::create('logbook', function (Blueprint $table) {
+        Schema::create('logbooks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('logbook', 255);
             $table->softDeletes();
