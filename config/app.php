db040c143be09103469f199bdf19136bb974cd52
Eugene Mah <eugenemah@gmail.com>
2018-11-30 16:57:56 -0500

    Remove laravelcollective aliases

--- config/app.php
+++ config/app.php
@@ -150,7 +150,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
